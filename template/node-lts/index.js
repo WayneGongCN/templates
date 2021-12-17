b725b67d26f452080081be304037c677e8938c52
unknown <wayne.gong.cn@hotmail.com>
2021-12-17 22:08:46 +0800

    chore: delete other files

--- template/node-lts/index.js
+++ template/node-lts/index.js
@@ -8,10 +8,20 @@ const handler = require("./function/handler");
 // middlewares
 app.use(bodyParser.json());
 
-
-// handler all method
+// handle all method
 app.all("/*", handler);
 
+// handle error
+app.use(function (err, req, res, next) {
+  if (res.headersSent) return next(err);
+  else {
+    const status = err.status || 500;
+    const code = err.code || -1;
+    const message = err.message || "";
+    const stack = err.stack || "";
+    return res.status(status).json({ code, message, stack });
+  }
+});
 
 // listen port
 const port = process.env.http_port || 3000;
